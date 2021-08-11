901a97a6c093257087dd45c7a4948c6bd742ad0a
Norhan1472 <nmohamed1472000@gmail.com>
2021-08-11 23:03:29 +0200

    Hello Branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'FirstScreen.dart';
 import 'SplashScreen.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: FirstScreen(),
     );
   }
 }
